1a910d6c4c0835341af0a90417f8747976ef2ddd
neru <ngocngocthuc@gmail.com>
2024-10-16 00:20:37 +0700

    1

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,9 +1,14 @@
 document.addEventListener("wheel", function (event) {
   if (event.deltaY !== 0) {
-    document.getElementById("main").scrollLeft += event.deltaY;
+    
+    if(!isOpen) {
+      document.getElementById("main").scrollLeft += event.deltaY;
+    }else{
+      document.getElementById("sub").scrollLeft += event.deltaY;
+    }
   }
 });
-
+let isOpen = false;
 const video = document.getElementById("scrollVideo");
 
 const observer = new IntersectionObserver((entries) => {
@@ -17,3 +22,17 @@ const observer = new IntersectionObserver((entries) => {
 }, { threshold: 0.5 });
 
 observer.observe(video);
+
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+const menu = document.getElementById("menu");
+
+openMenu.addEventListener("click", () => {
+  menu.classList.remove("translate-y-full");
+  isOpen = true;
+});
+
+closeMenu.addEventListener("click", () => {
+  menu.classList.add("translate-y-full");
+  isOpen = false;
+});
