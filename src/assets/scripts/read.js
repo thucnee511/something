a6693cf62584e937a7858853a5c039fc9790db55
neru <ngocngocthuc@gmail.com>
2025-01-03 02:33:57 +0700

    .

--- src/assets/scripts/read.js
+++ src/assets/scripts/read.js
@@ -1,10 +1,3 @@
-document.querySelectorAll(".remove__button--icon").forEach((btn) => {
-  btn.addEventListener("click", (event) => {
-    const id = event.target.parrentElement.getAttribute("data-id");
-    console.log(id);
-  });
-});
-
 const themeButton = document.querySelector("#themeButton");
 const fontButton = document.querySelector("#fontButton");
 const hightlightButton = document.querySelector("#hightlightButton");
